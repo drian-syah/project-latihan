822f03ef1f671867ddc9de771745b18ba187d8c3
drian-syah <mta99910@gmail.com>
2026-01-23 07:33:56 +0700

    perubahan v1

--- app/database/migration/001_create_users_table.php
+++ app/database/migration/001_create_users_table.php
@@ -4,8 +4,11 @@ return [
     "up" => "
         CREATE TABLE IF NOT EXISTS users (
             id INT AUTO_INCREMENT PRIMARY KEY,
+            username VARCHAR(100) UNIQUE,
             name VARCHAR(100),
-            email VARCHAR(100),
+            role ENUM('Admin', 'Siswa'),
+            email VARCHAR(100) UNIQUE,
+            password VARCHAR(255),
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         )
     ",
